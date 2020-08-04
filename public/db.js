c711576426e16847c7b2fca5f188dc0cf674d0ab
nhuanthi <tlam1288@gmail.com>
2020-08-04 15:29:02 -0700

    created db.js to create the new IndexedDB and transactions objectstore. Linked db.js to index.html

--- public/db.js
+++ public/db.js
@@ -1,3 +1,18 @@
 let db;
 // create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  // create object store called "pending" and set autoIncrement to true
+  const db = event.target.result;
+  const objectStore = db.createObjectStore("transactions");
+};
+
+request.onsuccess = function (event) {
+  //db = event.target.result;
+  console.log(request.result);
+};
+
+request.onerror = function (event) {
+  console.log("Woops! " + event.target.errorCode);
+};
